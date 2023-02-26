cde916c6c7cf589927279d531c10177f38626988
Ethan Pedersen <eped360@gmail.com>
2023-02-26 16:19:57 -0700

    Added basic front page with links to my socials, a profile picture, and a description

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,83 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+  <nav
+    class="flex mb-5 py-5 w-full max-w-full mx-auto px-auto justify-center bg-licorice rounded-md space-x-6"
+  >
+    <ul>
+      <button
+        class="bg-english-violet hover:bg-cool-gray text-white font-bold py-2 px-4 mx-4 rounded"
+      >
+        Home
+      </button>
+      <button
+        class="bg-english-violet hover:bg-cool-gray text-white font-bold py-2 px-4 mx-4 rounded"
+      >
+        Projects
+      </button>
+      <button
+        class="bg-english-violet hover:bg-cool-gray text-white font-bold py-2 px-4 mx-4 rounded"
+      >
+        Contact
+      </button>
+    </ul>
+  </nav>
+  <div class="text-center flex mt-10 justify-center flex-col">
+    <img
+      src="../public/Headshots-med.jpg"
+      class="img-fluid self-center rounded-full max-w-xs mb-4"
+      alt="Ethan"
+    />
+    <h1 class="text-4xl font-bold mb-2 text-white">Hi, I'm Ethan</h1>
+  </div>
+  <div class="flex flex-row space-x-2 mx-auto justify-center">
+    <a
+      href="https://github.com/ethanjped"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src="/github.svg"
+        class="img-fluid self-center rounded-full max-w-xs mb-4"
+        alt="GitHub"
+      />
     </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    <a
+      href="https://www.linkedin.com/in/ethanpedersen/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src="/linkedin.svg"
+        class="img-fluid self-center rounded-full max-w-xs mb-4"
+        alt="LinkedIn"
+      />
+    </a>
+    <a
+      href="https://twitter.com/pedersen_ethan"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src="/twitter.svg"
+        class="img-fluid self-center rounded-full max-w-xs mb-4"
+        alt="Twitter"
+      />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+  <p class="rounded-lg p-6 text-justify mx-auto max-w-4xl text-lg">
+    I want to design and build software solutions to solve problems. In my
+    present studies I am most interested in learning Sveltekit, Android app
+    development, and .NET applications. I currently work professionally in web
+    development using .NET ASP Web Forms and MVC, creating E-commerce sites and
+    developing custom CRM software. I plan on graduating in the spring of 2023
+    with Bachelors of Computer Science from the University of Utah.
+    <br />
+    Right now I am working on a senior project called TheraJournal. I am always looking
+    for new opportunities to expand my skillset and gain experience!
   </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
